581d362c175c45249c5646aea0245e6b1e491073
Joseph Mugisha <mugishaj@outlook.com>
2013-09-24 04:49:19 +0000

    added cheerioHtmlFile function

--- grader.js
+++ grader.js
@@ -22,3 +22,17 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertFileExists = function(infile){
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)){
+	console.log("%s does not exist. Exiting,", instr);
+	process.exit(1); //http://node.js.org/api/process.html#process_process_exit_code
+    }  
+    return instr;
+}
+
+var cheerioHtmlFile = function(htmlfile){
+    return cheerio.load(fs.readFileSync(htmlfile));
+}
+
