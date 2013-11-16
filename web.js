7e646a64cb1db920650ef98304d6d0239a038a6e
Joseph Mugisha <mugishaj@outlook.com>
2013-11-16 06:46:25 +0000

    :q  Please enter the commit message for your changes. Lines starting

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 
 var fs = require('fs');
-var fil = fs.readFileSync('index.html');
-var str = fil.toString();
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var htmlfile = "index.html"
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(str);
+    var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
